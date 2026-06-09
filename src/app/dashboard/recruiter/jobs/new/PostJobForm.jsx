574084cdf96da7fd4642519eace4e63169ef7e8e
ayshaaktersaima1 <ayshaaktersaima1@gmail.com>
2026-06-09 09:02:33 +0600

    did work for imgbb pics

--- src/app/dashboard/recruiter/jobs/new/PostJobForm.jsx
+++ src/app/dashboard/recruiter/jobs/new/PostJobForm.jsx
@@ -67,7 +67,7 @@ export default function PostJobForm({ company }) {
             isPubliclyVisible: true,
         };
 
-        console.log(jobData);
+        console.log(jobDataa);
 
         const res = await createJob(jobData);
 
